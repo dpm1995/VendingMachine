99294ae4f2cb3bb3e06f5b4efb279f3a94424198
Tom Medvitz <tom@techelevator.com>
2021-10-07 09:42:22 -0400

    Initial commit

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,9 @@
+package com.techelevator;
+
+// Vending Machine Command Line Interface application
+public class VendingMachineCLI {
+
+	public static void main(String[] args) {
+		// Make some objects here!
+	}
+}
